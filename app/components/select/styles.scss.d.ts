3ece28911b378bf097ddeade59a6f913004b47f5
Celebro dev <ed.proff@gmail.com>
2018-03-26 12:49:32 +0300

    update

--- app/components/select/styles.scss.d.ts
+++ app/components/select/styles.scss.d.ts
@@ -1,5 +1,5 @@
 export const select: string;
 export const isFocused: string;
 export const selectRtl: string;
-export const selectAnimationSpin: string;
 export const selectAnimationFadeIn: string;
+export const selectAnimationSpin: string;
