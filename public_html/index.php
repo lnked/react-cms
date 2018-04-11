5ae608262822d96d6563c322657a2d9843ebb2e5
Celebro dev <ed.proff@gmail.com>
2018-04-11 23:47:42 +0300

    update

--- public_html/index.php
+++ public_html/index.php
@@ -5,7 +5,7 @@
 
 // Set path constants
 define('FASTEST_BASE_PATH', dirname(__DIR__));
-define('FASTEST_VENDOR_PATH', FASTEST_BASE_PATH.'/vendor');
+define('FASTEST_VENDOR_PATH', FASTEST_BASE_PATH.'/fastest/vendors');
 
 // Load Composer's autoloader
 require_once FASTEST_VENDOR_PATH.'/autoload.php';
@@ -15,7 +15,7 @@ if (file_exists(FASTEST_BASE_PATH.'/.env')) {
     (new Dotenv\Dotenv(FASTEST_BASE_PATH))->load();
 }
 
-// Load and run Craft
-define('CRAFT_ENVIRONMENT', getenv('ENVIRONMENT') ?: 'production');
-$app = require FASTEST_VENDOR_PATH.'/craftcms/cms/bootstrap/web.php';
-$app->run();
+// // Load and run Craft
+// define('CRAFT_ENVIRONMENT', getenv('ENVIRONMENT') ?: 'production');
+// $app = require FASTEST_VENDOR_PATH.'/craftcms/cms/bootstrap/web.php';
+// $app->run();
